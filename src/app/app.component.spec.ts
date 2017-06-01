b80cb1028c8d123c59e80a2dc743757b6d78fddd
Mikolaj Zimny <mikolaj.zimny@briisk.co>
2017-06-01 16:17:42 +0200

    alert.component and auth guard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { By } from '@angular/platform-browser';
+// import { DebugElement } from '@angular/core';
+// import { AppComponent } from './app.component';
+// describe('AppComponent', () => {
+//   let comp: AppComponent;
+//   let fixture: ComponentFixture<AppComponent>;
+//   let de: DebugElement;
+//   let el: HTMLElement;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ AppComponent ],
+//     });
+
+//     fixture = TestBed.createComponent(AppComponent);
+//     comp = fixture.componentInstance;
+//     de = fixture.debugElement.query(By.css('h1'));
+//     el = de.nativeElement;
+//   });
+
+//   it('should display orginal title', () => {
+//     fixture.detectChanges();
+//     expect(el.textContent).toContain(comp.title);
+//   });
+// });
+
+
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -11,55 +39,31 @@ describe('AppComponent', () => {
   let de: DebugElement;
   let el: HTMLElement;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
-    });
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
     de = fixture.debugElement.query(By.css('h1'));
     el = de.nativeElement;
   });
 
+  it('should create the app', async(() => {
+    expect(comp).toBeTruthy();
+  }));
+
+  it(`should have as title 'app works!'`, async(() => {
+    expect(comp.title).toEqual('app works!');
+  }));
+
   it('should display orginal title', () => {
     fixture.detectChanges();
     expect(el.textContent).toContain(comp.title);
   });
-
-
 });
-
-
-// import { TestBed, async } from '@angular/core/testing';
-
-// import { AppComponent } from './app.component';
-
-// describe('AppComponent', () => {
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         AppComponent
-//       ],
-//     }).compileComponents();
-//   }));
-
-//   it('should create the app', async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app).toBeTruthy();
-//   }));
-
-//   it(`should have as title 'app works!'`, async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.debugElement.componentInstance;
-//     expect(app.title).toEqual('app works!');
-//   }));
-
-//   it('should render title in a h1 tag', async(() => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.debugElement.nativeElement;
-//     expect(compiled.querySelector('h1').textContent).toContain('app works!');
-//   }));
-// });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './_directives/alert/alert.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
